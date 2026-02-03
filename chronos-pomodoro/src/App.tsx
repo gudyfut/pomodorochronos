4a1fcfc7f09492c169de5aa081e432952d4a6397
gudyfut <murilorfm@gmail.com>
2026-02-03 03:56:29 -0300

    Primeiras criacoes
    app.tsx com style

---
+++ chronos-pomodoro/src/App.tsx
@@ -0,0 +1,14 @@
+import  './App.css'
+
+export function App() {
+    console.log("App component rendered");
+
+    return (
+        <>
+            <h1>Hello, Pomodoro Chronos!</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio iusto praesentium exercitationem velit debitis accusamus ut? Recusandae rerum accusamus mollitia. Deleniti doloremque deserunt pariatur neque molestiae esse distinctio blanditiis voluptatem. </p>
+        </>
+    );
+
+}
+
