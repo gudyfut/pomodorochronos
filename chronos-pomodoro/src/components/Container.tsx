75d4a10d186dd326a08456b486eaa79ebfbe0e89
gudyfut <murilorfm@gmail.com>
2026-02-08 13:14:23 -0300

    mais container

---
+++ chronos-pomodoro/src/components/Container.tsx
@@ -0,0 +1,15 @@
+import styles from './Container.module.css';
+
+// Definindo o tipo das props do componente Container
+type ContainerProps = { 
+    children: React.ReactNode; // Permite que o componente receba qualquer conteúdo como filho
+};
+
+export function Container({children}: ContainerProps) {
+    return (
+        <div className={styles.container}>
+            <div className={styles.content}>
+                {children}
+            </div>
+        </div>);
+}
